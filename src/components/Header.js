1ef789cc736baeef1f7bdc50f2fdbfc7f5c0a614
lentil09 <vance.kuikahi@gmail.com>
2024-02-21 18:01:32 -0800

    cleaned up Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,13 @@ import './Header.css';
 function Header() {
   return (
     <>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Create</p>
+      <header className="Header">
+        <img src={logo} className="Header-logo" alt="logo" />
+        <div className="Header-options">
+          <p>Create</p>
+          <p>View</p>
+          <p>Play</p>
+        </div>
       </header>
     </>
   );
