c82947dc3e170072144231f9a6cc2d16eb193458
lentil09 <vance.kuikahi@gmail.com>
2024-02-21 20:57:19 -0800

    making it look nicer

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import './Card.css'
 function Card( {name, type} ) {
   return (
     <div className="Card">
-      <img className="Card-Image" src="https://cdn.myanimelist.net/images/anime/1806/126216l.jpg"></img>
+      <img className="Card-Image" src="https://placehold.co/140x140/png"></img>
       <p>{name}</p>
       <p>{type}</p>
     </div>
