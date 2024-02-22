5d804452b3b61ee3326282addf76610da6f7c7fa
lentil09 <vance.kuikahi@gmail.com>
2024-02-21 18:17:18 -0800

    added body and fixed footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+function Footer() {
+  return (
+    <>
+      <footer className="Footer">
+        <p>Test</p>
+      </footer>
+    </>
+  )
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
+    <App />
+    <Footer />
   </React.StrictMode>
 );
 
