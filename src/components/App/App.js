a0eeb98ca38217663be60f45e4ca90440107c336
lentils <vancekuikahi@gmail.com>
2024-02-23 17:23:37 -1000

    using json server for input

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,27 @@
 import './App.css';
 import Card from '../Card/Card';
-import { useState } from 'react';
-import { inputs } from './Inputs' 
+import { useEffect, useState } from 'react';
+import { inputs } from './Inputs';
 
 function App() {
+  const [inputs, setInputs] = useState([]);
+  const getData = () => {
+    var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    }
+    fetch("http://localhost:8000/test", requestOptions)
+      .then((response) => response.json())
+      .then((result) => setInputs(result))
+      .then((error) => console.log("error", error));
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
   const [searchResults, setSearchResults] = useState(inputs);
+  useEffect(() => {setSearchResults(inputs)}, [inputs]); //this forces inputs as default
+
   const handleInputChange = (e) => {
     const searchTerm = e.target.value;
     const results = inputs.filter((card) => card.name.toLowerCase().includes(searchTerm.toLowerCase()));
